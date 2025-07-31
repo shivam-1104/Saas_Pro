949e8c7a6c3d0a9127446565a37102d1729dfb58
Shivam Jaiswal <jaiswalshivam22121@gmail.com>
2025-07-31 20:47:00 +0530

    more changes

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { PricingTable } from '@clerk/clerk-react'
 
 const Plan = () => {
   return (
@@ -6,7 +7,10 @@ const Plan = () => {
         <div className='text-center' >
             <h2 className='text-slate-700 text-[42px] font-semibold' >Choose Your Plan</h2>
             <p className='text-gray-500 max-w-lg mx-auto' >Start for free and scale up as you grow. Find the perfect plan for your content creation needs.</p>
-        </div>      
+        </div>   
+        <div className='mt-14 max-sm:mx-8' >
+            <PricingTable />
+        </div>   
     </div>
   )
 }
