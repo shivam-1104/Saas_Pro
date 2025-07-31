9f2d87e19eb80b441e5908a7c670db4201274cac
Shivam Jaiswal <jaiswalshivam22121@gmail.com>
2025-07-31 19:33:14 +0530

    my first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import AiTools from '../components/AiTools';
+import Testimonial from '../components/Testimonial';
+
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <AiTools />
+      <Testimonial />
+    </>
+  );
+}
+
+export default Home
