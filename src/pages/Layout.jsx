5e5921ef693790b56789f9128570d0dbc32b4977
Shivam Jaiswal <jaiswalshivam22121@gmail.com>
2025-08-04 20:44:42 +0530

    new changes

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,14 +3,16 @@ import { Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { Menu, X } from 'lucide-react';
 import Sidebar from '../components/Sidebar';
+import { SignIn ,useUser } from '@clerk/clerk-react';
 
 const Layout = () => {
   const navigate = useNavigate();
+  const { user } = useUser();
   const [sidebar, setSidebar] = useState(false)
-  return (
+  return user ? (
     <div className="flex flex-col items-start justify-start h-screen">
-      <nav className="w-full px-8 min-h-14 flex items-center h-screen">
-        <img src={assets.logo} alt="" onClick={() => navigate("/")} />
+      <nav className="w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200">
+        <img className='cursor-pointer w-32 sm:w-44' src={assets.logo} alt="" onClick={() => navigate("/")} />
         {sidebar ? (
           <X
             onClick={() => setSidebar(false)}
@@ -30,7 +32,11 @@ const Layout = () => {
         </div>
       </div>
     </div>
-  );
+  ) : (
+    <div className= 'flex items-center justify-center h-screen' >
+      <SignIn />
+    </div>
+  )
 }
 
 export default Layout
